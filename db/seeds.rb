a959c905a07eb38867b7584caf60dd2a7b41e95b
Hannah Winter <hstwinter@gmail.com>
2013-05-28 11:18:59 -0700

    added vote and contact models and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,42 +9,75 @@
 require 'rubygems'
 require 'faker'
 
-tag_array = [ 'children',
-              'substance abuse',
-              'alcoholism',
-              'ptsd',
-              'trauma',
-              'hospice',
-              'GLBTQ',
-              'employment',
-              'health-care',
-              'womens health',
-              'immigration services'
-              ]
+# Agency
 
-25.times do
-  name = Faker::Company.name
-  street = Faker::Address.street_address
-  city = Faker::Address.city
-  state = Faker::Address.state_abbr
-  phone = Faker::PhoneNumber.phone_number
-  short_description = Faker::Lorem.sentence
-  long_description = Faker::Lorem.paragraph
-  score = rand(25)
-  score1 = rand(25)
-  score2 = rand(25)
-  tags = tag_array.sample(3).join(",")
+# tag_array = [ 'children',
+#               'substance abuse',
+#               'alcoholism',
+#               'ptsd',
+#               'trauma',
+#               'hospice',
+#               'GLBTQ',
+#               'employment',
+#               'health-care',
+#               'womens health',
+#               'immigration services'
+#               ]
 
-  agency = Agency.create( :name => name,
-                          :street => street,
-                          :city => city,
-                          :state => state,
-                          :phone => phone,
-                          :short_description => short_description,
-                          :long_description => long_description,
-                          :in_house_rating => score,
-                          :overall_rating => score1,
-                          :client_rating => score2,
-                          :tag => tags
-                          )
-end
+# 25.times do
+#   name = Faker::Company.name
+#   street = Faker::Address.street_address
+#   city = Faker::Address.city
+#   state = Faker::Address.state_abbr
+#   phone = Faker::PhoneNumber.phone_number
+#   short_description = Faker::Lorem.sentence
+#   long_description = Faker::Lorem.paragraph
+#   score = rand(25)
+#   score1 = rand(25)
+#   score2 = rand(25)
+#   tags = tag_array.sample(3).join(",")
+
+#   agency = Agency.create( :name => name,
+#                           :street => street,
+#                           :city => city,
+#                           :state => state,
+#                           :phone => phone,
+#                           :short_description => short_description,
+#                           :long_description => long_description,
+#                           :in_house_rating => score,
+#                           :overall_rating => score1,
+#                           :client_rating => score2,
+#                           :tag => tags
+#                           )
+# end
+
+# Contacts
+
+# 3.times do
+#   name = Faker::Name.name
+#   phone = Faker::PhoneNumber.phone_number
+#   text = Faker::Lorem.paragraph
+#   agency_id = rand(25) + 1
+
+#   Contact.create(:user_id => 3,
+#                  :agency_id => agency_id,
+#                  :name => name,
+#                  :phone => phone,
+#                  :notes => text
+#                 )
+# end
+
+# Votes
+
+# 100.times do
+#   vote = rand(5) + 1
+#   user_id = rand(25) + 1
+#   agency_id = rand(25) + 1
+#   user_agency_id = rand(25) + 1
+
+#   Vote.create(:user_id => user_id,
+#               :user_agency_id => user_agency_id,
+#               :agency_id => agency_id,
+#               :vote => vote
+#              )
+# end
