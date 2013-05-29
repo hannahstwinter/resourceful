d5f39bc3558efac06b465ad30878303760f0fa92
Hannah Winter <hstwinter@gmail.com>
2013-05-29 12:54:50 -0700

    ability to add agency, minor ui adjustments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,6 @@ class PagesController < ApplicationController
 
   def index
     @agencies = Agency.search(params[:search], params[:rating])
-    # if search
-    #   @agencies = Agency.search #find(:all, :conditions => ['name LIKE ?', "%#{search}%"])
-    # else
-    #   @agencies = Agency.order("out_of_house DESC").limit(5)
-    # end
-
-    # @agencies = Agency.order("out_of_house DESC").limit(5)
-
     if current_user && current_user.agency_id != nil
       @user_agency = Agency.find(current_user.agency_id)
     end
