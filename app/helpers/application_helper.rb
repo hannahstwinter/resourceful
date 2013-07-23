1f96c3729aff8f091047d4aa49176cc35d8b3bda
hannahstwinter <hstwinter@gmail.com>
2013-07-23 16:26:30 -0400

    css and flash modifications

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,11 @@
 module ApplicationHelper
 
   def fading_flash_notice
-    # note: you must have a div with id='notices' or rename the div appended to below with your element which
-    # is the container for the flash messages
     return '' if !flash[:notice]
     notice_id = rand.to_s.gsub(/\./, '')
     notice = <<-EOF
       $('#notices').append("<div id='#{notice_id}' class='flash_notice'>#{flash[:notice]}</div>");
-      $("##{notice_id}").delay(5000).slideUp();
+      $("##{notice_id}").css({'padding':'10px 10px 10px 20px'}).delay(5000).slideUp();
     EOF
     notice.html_safe
   end
