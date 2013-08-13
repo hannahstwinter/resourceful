95eca86dfe013abf546b1499c43e6eb1896c7339
hannahstwinter <hstwinter@gmail.com>
2013-08-13 11:47:48 -0400

    cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,10 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:error] = "Access denied!"
+    redirect_to root_url
+  end
+
   helper_method :current_user
 end
