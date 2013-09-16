a89af760a45670b88113d0ea2e935ec8645c7123
hannahstwinter <hstwinter@gmail.com>
2013-09-16 14:17:44 -0400

    heroku config

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,16 +11,19 @@ require 'faker'
 
 # NRVCS
 
-Agency.create(:name => "New River Valley Community Services (NRVCS)",
-              :street => "1006 E Main St.",
-              :city => "Pulaski",
-              :state => "Virginia",
-              :short_description => "NRVCS provides crisis & family services, adult recovery & habilitation services, financial & administrative support services." ,
-              :long_description => "New River Valley Community Services (NRVCS) is a public provider of behavioral health services to residents of the New River Valley. The agency serves children, adults and families by providing community-based programs for mental health, intellectual disabilities, substance use disorders, and related prevention services. Also, one of the workers there is really hot. Really, really hot.",
-              :url => "www.nrvcs.org",
-              :phone => "(540)994-5023",
-              :tag => "child,family,rehabilitation,financial,crisis"
-              )
+nrvcs = Agency.create!(:name => "New River Valley Community Services (NRVCS)",
+                       :street => "1006 E Main St.",
+                       :city => "Pulaski",
+                       :state => "Virginia",
+                       :short_description => "NRVCS provides crisis & family services, adult recovery & habilitation services, financial & administrative support services." ,
+                       :long_description => "New River Valley Community Services (NRVCS) is a public provider of behavioral health services to residents of the New River Valley. The agency serves children, adults and families by providing community-based programs for mental health, intellectual disabilities, substance use disorders, and related prevention services. Also, one of the workers there is really hot. Really, really hot.",
+                       :url => "www.nrvcs.org",
+                       :phone => "(540)994-5023",
+                       :tag => "child,family,rehabilitation,financial,crisis"
+                       )
+
+User.create!(name: "Hannah Winter", email: "hstwinter@gmail.com", password: "", agency_id: nrvcs.id, authz: "admin")
+User.create!(name: "Sarah Winter", email: "winter.hs@gmail.com", password: "", agency_id: nrvcs.id, authz: "admin")
 
 # # Review
 
