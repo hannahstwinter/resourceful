5472710c20fc6173a422f94e23433a195a18a3ea
hannahstwinter <hstwinter@gmail.com>
2013-09-16 13:50:18 -0400

    fixing heroku maybe...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   def current_user
-    session[:user_id] = nil
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
