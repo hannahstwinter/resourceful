f15a6b6fab56283a9bfaf7e8e83b59067799f7bb
hannahstwinter <hstwinter@gmail.com>
2013-09-16 13:46:44 -0400

    still breaking heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     session[:user_id] = nil
-    # @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
   rescue_from CanCan::AccessDenied do |exception|
